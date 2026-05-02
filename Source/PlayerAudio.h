8d6572b95a14e9a7bcff8e882afae2444e0bfaea
Y. Nagy <youssefnaggi@gmail.com>
2026-05-02 20:24:39 +0300

    Update PlayerAudio.h

--- Source/PlayerAudio.h
+++ Source/PlayerAudio.h
@@ -17,14 +17,14 @@ public:
     void setPosition(double pos);
     double getPosition() const;
     double getLength() const;
-    void setLooping(bool shouldLoop); //mennna
-    juce::String getFormattedLength() const;//nagy
-    void setLoopPoints(double start, double end); //new menna
-    void enableABLoop(bool enable); //new menna
-    void checkAndLoopSegment(); //new menna
-    void setSpeed(double ratio);//jana
+    void setLooping(bool shouldLoop); 
+    juce::String getFormattedLength() const;
+    void setLoopPoints(double start, double end); 
+    void enableABLoop(bool enable); 
+    void checkAndLoopSegment(); 
+    void setSpeed(double ratio);
 
-    juce::AudioTransportSource& getTransportSource() { return transportSource; }//jana
+    juce::AudioTransportSource& getTransportSource() { return transportSource; }
 
 
 
@@ -32,10 +32,10 @@ private:
     juce::AudioFormatManager formatManager;
     std::unique_ptr<juce::AudioFormatReaderSource> readerSource;
     juce::AudioTransportSource transportSource;
-    bool isLooping = false; //menna
-    double loopStart = 0.0; // new menna
-    double loopEnd = 0.0; // new menna
-    bool isABLooping = false; // new menna
+    bool isLooping = false; 
+    double loopStart = 0.0; 
+    double loopEnd = 0.0; 
+    bool isABLooping = false; 
 
 
 
